aae47fde06b811b1210adcb71e89a1893d86d4bf
Christopher Kotfila <kotfic@gmail.com>
2012-03-19 10:36:16 -0400

    adding ticket accordion

--- js/assembla_spaces.js
+++ js/assembla_spaces.js
@@ -3,15 +3,24 @@ var AssemblaApp = chrome.extension.getBackgroundPage().AssemblaApp;
 
 jQuery(document).ready(function() {
 
-   // When clicking a space link, (wiki, files, time, etc) - Open a new tab for it
-   jQuery('.space-link').click(function() {
-       chrome.tabs.create({url: "http://" + jQuery(this).attr('href')});
-       return false;
-   });
+    
 
-   // Grab the spaces in assembla, retrieved from the background page.
-
-
+    // When clicking a space link, (wiki, files, time, etc) - Open a new tab for it
+    jQuery('.space-link').click(function() {
+	chrome.tabs.create({url: "http://" + jQuery(this).attr('href')});
+	return false;
+    });
+    debugger;
+    _.each( AssemblaApp.getActiveTickets(), function(ticket) {
+	var h3 = jQuery("<h3>");
+	var div = jQuery("<div>");
+	h3.append(jQuery("<a href='#'>" + ticket.number + ": " + ticket.summary + "</a>") );                         
+	div.append(jQuery("<span>" + AssemblaApp.getSpaceName(ticket.space_id) + "</span>") );
+        jQuery("#tabs-1 #ticket-div").append(h3).append(div);
+    });
+			   
+    jQuery("#tabs-1 #ticket-div").accordion();
+			   
    // Populate select for spaces with assemblaSpaces
     jQuery.each(AssemblaApp.getActiveSpaces(), function(i, item) {
 	jQuery('#tabs-2 #assembla-spaces').append('<option value="' + item.id + '">' + item.name + '</option>');
