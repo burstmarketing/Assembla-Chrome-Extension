0ba0874dfd1f970e98c455e787d04f5894dc6207
Christopher Kotfila <kotfic@gmail.com>
2012-03-30 17:16:45 -0400

    adding backbone sync support for htauthentication

--- js/assembla_timelogs.js
+++ js/assembla_timelogs.js
@@ -1,6 +1,6 @@
-var ticket_tab_accordion_view;
+var time_log_tab_accordion_view;
 var AssemblaApp = chrome.extension.getBackgroundPage().AssemblaApp;
-
+/*
 AssemblaApp.Views.Spaces_Tab = Backbone.View.extend({
     initialize : function() {
 	this.ticket_list = new AssemblaApp.Views.Ticket_Tab_Accordion({});
@@ -116,10 +116,63 @@ AssemblaApp.Views.Ticket_Accordion_Element = Backbone.View.extend({
     }
 });
 
+*/
+
+
+AssemblaApp.Views.Time_Log_Accordion = Backbone.View.extend({
+    initialize : function(){
+        this._timelogViews = [];
+
+        AssemblaApp.dispatcher.on("bootstrapActiveTimeLogs:loaded", function () {
+	    this.collection = AssemblaApp.getActiveTimeLogs();
+            this.render();
+        }, this);
+
+    },
+
+    render : function() {
+	jQuery(this.el).empty();
+	if( this.collection.length ){
+	    this.collection.each( function(timelog){
+		var view =  new AssemblaApp.Views.Time_Log_Accordion_Element({
+		    el : this.el,
+		    model : timelog
+		});
+		this._timelogViews.push(view);
+		view.render();
+
+            }, this );
+
+	    jQuery(this.el).accordion({ collapsible: true,
+					autoHeight: false,
+					active: false });
+
+	}
+	return this;
+    }
+});
+
+AssemblaApp.Views.Time_Log_Accordion_Element = Backbone.View.extend({
+    render : function() {
+        var tmplt = _.template(jQuery("#timelog-accordion").html() );
+        jQuery(this.el).append( tmplt({ timelog : this.model.toJSON() }) );
+
+	return this;
+    }
+});
+
 
 
 jQuery(document).ready(function() {
 
+    time_log_tab_accordion_view = new AssemblaApp.Views.Time_Log_Accordion({
+	el : "#tabs-3 #previous_time_logs",
+	collection : AssemblaApp.getActiveTimeLogs()
+    });
+
+    time_log_tab_accordion_view.render();
+
+/*
     // When clicking a space link, (wiki, files, time, etc) - Open a new tab for it
     jQuery('.space-link').live( "click", function() {
 	chrome.tabs.create({url: "http://" + jQuery(this).attr('href')});
@@ -140,7 +193,7 @@ jQuery(document).ready(function() {
 
     ticket_tab_accordion_view.render();
     spaces_tab.render();
-
+*/
 
 
 });
