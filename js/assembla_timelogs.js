5aed2028ee16475bace6817472c9877bafb576a9
Christopher Kotfila <kotfic@gmail.com>
2012-04-01 13:56:52 -0400

    adding 'with' functionality to timelog display' '

--- js/assembla_timelogs.js
+++ js/assembla_timelogs.js
@@ -5,8 +5,8 @@ AssemblaApp.Views.Time_Log_Accordion = Backbone.View.extend({
     initialize : function(){
         this._timelogViews = [];
 
-        AssemblaApp.getActiveTimeLogs().on("change", function () {
-	    this.collection = AssemblaApp.getActiveTimeLogs();
+        AssemblaApp.getTimeLogs().on("change", function () {
+	    this.collection = AssemblaApp.TimeLogs();
             this.render();
         }, this);
 
@@ -25,6 +25,32 @@ AssemblaApp.Views.Time_Log_Accordion = Backbone.View.extend({
 
             }, this );
 
+	    //  we're not garunteed that the space and ticket for each
+	    //  timelog will be in our cached spaces so we render them
+	    //  with loading spinners and then set the actual values using
+	    //  the 'with'  functions which should load the correct content
+	    //  immediately if the space/ticket is in the cache,  or call
+	    //  out to the API to get the right space/ticket info.
+
+	    // set up an element variable to be passed into all the
+	    // 'with' function closures.
+	    var element = this.el;
+
+	    // 'this' referes to the space model
+
+	    this.collection.each( function(tl){
+		AssemblaApp.withSpace( tl.get("space_id"), function(){
+		    console.log("space:foo");
+		    // 'this' referes to the ticket model
+		    this.withTicket( tl.get("ticket_id"), function(){
+			console.log("ticket:foo");
+			jQuery(element).find("h3." + tl.get("id") + " span.timelog-name" ).empty()
+			    .html( this.get("number") + ": " + this.get("summary"));
+
+		    });
+		});
+	    }, this );
+
 	    jQuery(this.el).accordion({ collapsible: true,
 					autoHeight: false,
 					active: false });
@@ -36,8 +62,14 @@ AssemblaApp.Views.Time_Log_Accordion = Backbone.View.extend({
 
 AssemblaApp.Views.Time_Log_Accordion_Element = Backbone.View.extend({
     render : function() {
+	var tl = this.model.toJSON();
+
+	tl.ticket_name = "<img src='img/ajax-loader-spinner-small.gif'/>";
+
+
         var tmplt = _.template(jQuery("#timelog-accordion").html() );
-        jQuery(this.el).append( tmplt({ timelog : this.model.toJSON() }) );
+        jQuery(this.el).append( tmplt({ timelog : tl }) );
+
 
 	return this;
     }
@@ -49,7 +81,7 @@ jQuery(document).ready(function() {
 
     time_log_tab_accordion_view = new AssemblaApp.Views.Time_Log_Accordion({
 	el : "#tabs-3 #previous_time_logs",
-	collection : AssemblaApp.getActiveTimeLogs()
+	collection : AssemblaApp.getTimeLogs()
     });
 
     time_log_tab_accordion_view.render();
